33e937c0dd941a75b37495d3f058179e18902f2f
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-04-29 22:36:20 +0200

    Test JPA query methods

--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/indexController.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/indexController.java
@@ -2,12 +2,31 @@ package pl.mateuszkoczan.springframework.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mateuszkoczan.springframework.recipeapp.domains.Category;
+import pl.mateuszkoczan.springframework.recipeapp.domains.UnitOfMeasure;
+import pl.mateuszkoczan.springframework.recipeapp.repositories.CategoryRepository;
+import pl.mateuszkoczan.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class indexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package pl.mateuszkoczan.springframework.recipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import pl.mateuszkoczan.springframework.recipeapp.domains.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
