f9759d8c79df1d39cad0b61a26d6146eb95bec25
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-04-30 19:24:28 +0200

    Update indexController class and index.html

--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/indexController.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/indexController.java
@@ -1,13 +1,22 @@
 package pl.mateuszkoczan.springframework.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mateuszkoczan.springframework.recipeapp.servies.RecipeService;
 
 @Controller
 public class indexController {
+    private final RecipeService recipeService;
+
+    public indexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping({"", "/", "index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
