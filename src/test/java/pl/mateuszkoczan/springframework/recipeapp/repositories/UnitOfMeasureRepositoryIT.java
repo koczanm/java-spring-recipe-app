5b9c182ca7c13794d89744dff08ec84b0e6df390
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-05-01 17:56:09 +0200

    Add integration test

---
+++ src/test/java/pl/mateuszkoczan/springframework/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package pl.mateuszkoczan.springframework.recipeapp.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.mateuszkoczan.springframework.recipeapp.domains.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> teaspoonUOMOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", teaspoonUOMOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescription2() {
+        Optional<UnitOfMeasure> teaspoonUOMOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", teaspoonUOMOptional.get().getDescription());
+    }
+}
