de9497f4df0395fa225dc8294fccb2b04b75da78
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-07-19 16:30:01 +0200

    Add methods for creating and updating a recipe in RecipeController

--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/controllers/RecipeController.java
@@ -2,8 +2,8 @@ package pl.mateuszkoczan.springframework.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pl.mateuszkoczan.springframework.recipeapp.commands.RecipeCommand;
 import pl.mateuszkoczan.springframework.recipeapp.services.RecipeService;
 
 @Controller
@@ -14,10 +14,32 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
-    public String showById(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+    @RequestMapping("/recipe/{id}/show")
+    public String showRecipeById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findRecipeById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String createRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
     }
 }
