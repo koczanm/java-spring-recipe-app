82f233a0e44464d0568417ae60fff281fe4f032c
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-07-20 11:50:22 +0200

    Update RecipeService interface and implementation

--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/services/RecipeServiceImpl.java
@@ -64,4 +64,9 @@ public class RecipeServiceImpl implements RecipeService {
 
         return recipeToRecipeCommand.convert(savedRecipe);
     }
+
+    @Override
+    public void deleteById(Long id) {
+        recipeRepository.deleteById(id);
+    }
 }
