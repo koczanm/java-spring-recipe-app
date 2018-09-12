68f6b256575c3aa658d080524c504f0d40bda098
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-12 13:35:13 +0200

    Add saving an image to db

--- src/main/java/pl/mateuszkoczan/springframework/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/pl/mateuszkoczan/springframework/recipeapp/services/ImageServiceImpl.java
@@ -3,13 +3,41 @@ package pl.mateuszkoczan.springframework.recipeapp.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import pl.mateuszkoczan.springframework.recipeapp.domains.Recipe;
+import pl.mateuszkoczan.springframework.recipeapp.repositories.RecipeRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObject = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (Byte b : file.getBytes()) {
+                byteObject[i++]  =b;
+            }
+
+            recipe.setImage(byteObject);
+
+            recipeRepository.save(recipe);
+        } catch (IOException ex) {
+            // todo handle better
+            log.error("Error occured", ex);
+
+            ex.printStackTrace();
+        }
     }
 }
